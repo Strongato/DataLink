49caaa379e82ba0ce53af880dbf0d2b70e9e35a5
Strongato <54669732+Strongato@users.noreply.github.com>
2024-09-03 22:30:46 +0200

    Update cli_usage.h

--- include/cli/cli_usage.h
+++ include/cli/cli_usage.h
@@ -2,6 +2,6 @@
 #define CLI_USAGE_H
 
 void print_usage(char* argv[]);
-int validate_args_and_print_usage(char* argv[], const unsigned short port, const char* const hostarg, const char* const employee_string, const char* const update_string, const bool remove);
+int validate_args_and_print_usage(char* argv[], const unsigned short port, const char* const hostarg, const char* const employee_string, const char* const update_string, const bool remove, const bool add);
 
 #endif

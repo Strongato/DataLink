b09174106dd4d4e24be07c1c621cdadd68cef6d6
Strongato <54669732+Strongato@users.noreply.github.com>
2024-09-13 14:51:03 +0200

    Update srvpoll.h

--- include/srv/srvpoll.h
+++ include/srv/srvpoll.h
@@ -29,10 +29,7 @@ void init_clients(clientstate_t* const states);
 int find_free_slot(const clientstate_t* const states);
 int find_slot_by_fd(const clientstate_t* const states, const int fd);
 
-
 void fsm_general_reply(clientstate_t* const client, dbproto_hdr_t* const hdr, const dbproto_type_e type);
-void fsm_general_reply_err(clientstate_t* const client, dbproto_hdr_t* const hdr);
-void fsm_general_reply_missing_err(clientstate_t* const client, dbproto_hdr_t* const hdr);
 
 void fsm_reply_hello(clientstate_t* const client, dbproto_hdr_t* const hdr);
 void fsm_reply_hello_err(clientstate_t* const client, dbproto_hdr_t* const hdr);
@@ -42,6 +39,8 @@ void fsm_reply_add_err(clientstate_t* const client, dbproto_hdr_t* const hdr);
 
 void fsm_reply_update(clientstate_t* const client, dbproto_hdr_t* const hdr);
 void fsm_reply_update_err(clientstate_t* const client, dbproto_hdr_t* const hdr);
+
+void fsm_reply_missing_err(clientstate_t* const client, dbproto_hdr_t* const hdr);
 
 void send_employees(const struct dbheader_t* const dbhdr, struct employee_t* const employees, clientstate_t* const client);
 void handle_client_fsm(struct dbheader_t* const dbhdr, struct employee_t** employees, clientstate_t* client, int dbfd, const char* const filepath);
